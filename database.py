5ca517eca250e84fa210cc41fe900ff857203f74
JeremyMarlinghaus <jmarl013@fiu.edu>
2026-03-31 15:00:38 -0400

    Update database.py

--- database.py
+++ database.py
@@ -1,1 +1,39 @@
+import mysql.connector
+from mysql.connector import Error
 
+def get_db_connection():
+
+  connection = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="YOUR_PASSWORD",
+    database="job_tracker"
+  )
+  return connection
+
+def execute_query(query, params=None, fetchone=False, fetchall=False, commit=False):
+
+connection = None
+cursor = None
+result =None
+
+try: connection = get_db_connection()
+     cursor = connection.cursor(dictionary=True)
+        cursor.execute(query, params or ())
+
+        if commit:
+            connection.commit()
+            result = cursor.lastrowid
+        elif fetchone:
+            result = cursor.fetchone()
+        elif fetchall:
+            result = cursor.fetchall()
+
+    finally:
+        if cursor is not None:
+            cursor.close()
+        if connection is not None and connection.is_connected():
+            connection.close()
+
+    return result
+  
